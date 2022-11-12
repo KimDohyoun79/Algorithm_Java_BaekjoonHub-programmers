4cf7375e63943110e2653bbc8f89db4dbeec752e
KimDohyoun79 <dokim@student.42seoul.kr>
2022-11-12 21:20:43 +0900

    [Silver III] Title: 1, Time: 128 ms, Memory: 14112 KB -BaekjoonHub

---
+++ 백준/Silver/4375. 1/1.java
@@ -0,0 +1,29 @@
+import java.io.*;
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String str = null;
+        while ((str = br.readLine()) != null){
+            int num = Integer.parseInt(str);
+
+            long i = 1;
+            int len = 1;
+
+            while (i % num != 0) {
+                i %= num;
+                i = i * 10 + 1;
+                len++;
+            }
+            bw.write(String.valueOf(len)+"\n");
+            bw.flush();
+        }
+        br.close();
+        bw.close();
+
+    }
+}
