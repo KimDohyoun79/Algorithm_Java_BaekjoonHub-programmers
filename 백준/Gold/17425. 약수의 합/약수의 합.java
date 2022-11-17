325ad5f352209dddc993f6ddfcbbb0d57547f88b
KimDohyoun79 <dokim@student.42seoul.kr>
2022-11-18 01:42:26 +0900

    [Gold IV] Title: 약수의 합, Time: 524 ms, Memory: 46276 KB -BaekjoonHub

--- 백준/Gold/17425. 약수의 합/약수의 합.java
+++ 백준/Gold/17425. 약수의 합/약수의 합.java
@@ -1,24 +1,47 @@
-
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 
-public class Main {
-    final static int MAX_SIZE = 1000001;
-    public static void main(String[] args) throws Exception{
+/*
+- 출력 문제도 있다.. 문제 설명 출력 부분에서 한줄에 하나씩 출력하라고 말한다..
+ => 예제를 복사해서 붙여넣기하여 컴파일하면 좀 엉키게 출력이 된다.
+1
+2
+10
+70
+10000 1
+4
+87
+4065
+
+82256014
+
+Process finished with exit code 0
+
+ */
+class Main {
+
+    public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        long[] dp = new long[MAX_SIZE];
+
         int t = Integer.parseInt(br.readLine());
-        Arrays.fill(dp,1);
-        for (int i = 2; i < MAX_SIZE; i++){
-            for (int j = 0; j * i < MAX_SIZE; j++){
-                dp[j * i] += i;
+
+        long[] arr = new long[1_000_001];
+        Arrays.fill(arr, 1);
+        arr[0] = 0;
+        
+        for (int i = 2; i < 1_000_001; i++) {
+            for (int j = 1; j * i < 1_000_001; j++) {
+                arr[i * j] += i;
             }
-            dp[i] = dp[i-1] + dp[i];
+            arr[i] = arr[i - 1] + arr[i];
         }
-        while (t>0){
-            t--;
-            sb.append(dp[Integer.parseInt(br.readLine())] + "\n");
+
+        StringBuilder sb = new StringBuilder();
+        while (t-- > 0){
+            sb.append(arr[Integer.parseInt(br.readLine())]+"\n");
         }
         System.out.println(sb);
     }
