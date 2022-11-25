c2b8424649360e375788affd31f425c18156c974
KimDohyoun79 <dokim@student.42seoul.kr>
2022-11-25 15:31:08 +0900

    [level 1] Title: 예산, Time: 0.47 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12982. 예산/예산.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+        int sum = 0;
+
+        Arrays.sort(d);
+
+        for (int i = 0; i < d.length; i++) {
+            if(sum<=budget){
+                sum += d[i];
+                answer++;
+            }
+        }
+
+        if (sum <= budget)
+            return answer;
+        else {
+            return answer - 1;
+        }
+    }
+}
