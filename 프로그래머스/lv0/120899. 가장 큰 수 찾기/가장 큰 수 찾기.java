048f9680e9fc4e9f51f948e8127a526073d80b3e
KimDohyoun79 <dokim@student.42seoul.kr>
2022-11-30 09:44:26 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.03 ms, Memory: 71.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
+++ 프로그래머스/lv0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
@@ -1,7 +1,9 @@
 class Solution {
     public int[] solution(int[] array) {
+        
         int[] answer = new int[2];
         int max = -1;
+        
         for (int i = 0; i < array.length; i++) {
             max = Math.max(max, array[i]);
         }
@@ -12,6 +14,7 @@ class Solution {
                 answer[1] = i;
             }
         }
+        
         return answer;
 
     }
