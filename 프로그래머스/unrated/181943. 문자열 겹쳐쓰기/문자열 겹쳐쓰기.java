44568438930cc58f06a20284124a4f8726cda174
KimDohyoun79 <dogdo5726@gmail.com>
2023-08-28 10:59:58 +0900

    [unrated] Title: 문자열 겹쳐쓰기, Time: 17.59 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -1,16 +1,7 @@
 class Solution {
     public String solution(String my_string, String overwrite_string, int s) {
-        StringBuilder answer = new StringBuilder();
-
-        for (int i = 0; i < my_string.length(); i++) {
-            if (i == s) {
-                answer.append(overwrite_string);
-                i += overwrite_string.length() - 1;
-            } else {
-                answer.append(my_string.charAt(i));
-            }
-        }
-
-        return answer.toString();
+        String first = my_string.substring(0, s);
+        String end = my_string.substring(overwrite_string.length() + s);
+        return first + overwrite_string + end;
     }
 }
