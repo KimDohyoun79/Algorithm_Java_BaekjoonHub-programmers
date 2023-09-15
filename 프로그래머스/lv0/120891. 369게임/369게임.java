713a17008f3a960a2c6acbb5f272f207cf0bebae
KimDohyoun79 <dogdo5726@gmail.com>
2023-09-15 22:53:08 +0900

    [level 0] Title: 369게임, Time: 0.03 ms, Memory: 79 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120891. 369게임/369게임.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int order) {
+        int answer = 0;
+        
+        while(order > 0){
+            int n = order % 10;
+            if(n != 0 && n % 3 == 0)
+                answer++;
+            
+            order /= 10;
+        }
+        
+        return answer;
+    }
+}
