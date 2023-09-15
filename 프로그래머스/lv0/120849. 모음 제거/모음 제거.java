dfa3478b325aa98a00499cf13ef460583d8c9fce
KimDohyoun79 <dogdo5726@gmail.com>
2023-09-15 11:04:34 +0900

    [level 0] Title: 모음 제거, Time: 0.12 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.java
@@ -1,21 +1,26 @@
 class Solution {
     public String solution(String my_string) {
-        String answer = "";
-        String[] strs = my_string.split("");
-        
-        for(String s : strs){
-            
-            switch(s){
-                case("a") : break;
-                case("e") : break;
-                case("i") : break;
-                case("o") : break;
-                case("u") : break;
-                default : answer += s;
-            }
-            
-        }
-        
-        return answer;
+        return my_string.replaceAll("[aeiou]", "");
     }
 }
+
+// #1
+// class Solution {
+//     public String solution(String my_string) {
+//         String answer = "";
+//         String[] strs = my_string.split("");
+        
+//         for(String s : strs){
+            
+//             switch(s){
+//                 case("a") : break;
+//                 case("e") : break;
+//                 case("i") : break;
+//                 case("o") : break;
+//                 case("u") : break;
+//                 default : answer += s;
+//             }
+//         }
+//         return answer;
+//     }
+// }
