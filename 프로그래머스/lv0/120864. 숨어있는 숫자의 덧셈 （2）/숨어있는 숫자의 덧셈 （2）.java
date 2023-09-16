a6f3dead8bfeaeb7fac5af3ffd77f3ffabdf46b5
KimDohyoun79 <dogdo5726@gmail.com>
2023-09-17 01:22:31 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.39 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
+++ 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
@@ -1,23 +1,40 @@
 class Solution {
     public int solution(String my_string) {
-        int answer = 0;
-        String s = "";
-        int i = 0;
         
-        while(i < my_string.length()){
-            
-            // 문자가 정수인 경우 문자열에 저장
-            if(Character.isDigit(my_string.charAt(i))){
-                s += my_string.charAt(i);
-            }
-            // 문자가 정수가 아닌 경우 이전 정수를 저장한 문자열 연산
-            else if(s.length() > 0){
-                answer += Integer.valueOf(s);
-                s = "";
-            }
-            i++;
+        // # 2
+        int answer = 0;
+        String[] numArr = my_string.replaceAll("[^0-9]", " ").split(" ");
+        
+ 
+        for(String s : numArr){
+                   System.out.println(s);
+            answer += s.equals("") ? 0 : Integer.valueOf(s);
         }
-        // 문자열 마지막에 정수가 있을 경우 위에서 처리를 못하여 마지막에 확인 처리
-        return s.length() > 0 ? answer += Integer.valueOf(s) : answer;
+        
+        return answer;
+        
+        
+        
+        
+            // # 1
+//         int answer = 0;
+//         String s = "";
+//         int i = 0;
+        
+//         while(i < my_string.length()){
+            
+//             // 문자가 정수인 경우 문자열에 저장
+//             if(Character.isDigit(my_string.charAt(i))){
+//                 s += my_string.charAt(i);
+//             }
+//             // 문자가 정수가 아닌 경우 이전 정수를 저장한 문자열 연산
+//             else if(s.length() > 0){
+//                 answer += Integer.valueOf(s);
+//                 s = "";
+//             }
+//             i++;
+//         }
+//         // 문자열 마지막에 정수가 있을 경우 위에서 처리를 못하여 마지막에 확인 처리
+//         return s.length() > 0 ? answer += Integer.valueOf(s) : answer;
     }
 }
