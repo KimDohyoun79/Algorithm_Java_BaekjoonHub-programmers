1a3a5981afa4a2fd7d737187cd257e1d085a7041
KimDohyoun79 <dogdo5726@gmail.com>
2023-09-25 22:35:30 +0900

    [unrated] Title: 문자열 바꿔서 찾기, Time: 0.04 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
+++ 프로그래머스/unrated/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
@@ -4,7 +4,7 @@ class Solution {
         StringBuilder sb = new StringBuilder();
         
         for(char c : pat.toCharArray())
-            sb.append(c == 'A' ? "B" : "A");
+            sb.append(c == 'A' ? 'B' : 'A');
         
         return myString.contains(sb.toString()) ? 1 : 0;
     }
