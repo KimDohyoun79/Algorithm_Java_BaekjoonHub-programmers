e456f6ba7ee89155669a34148f424cda7f3142b6
KimDohyoun79 <dogdo5726@gmail.com>
2023-09-30 20:58:13 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.02 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.java
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -3,17 +3,9 @@ class Solution {
         int answer = 0;
         if(common[1] - common[0] == common[2] - common[1])
             answer = common[common.length - 1] + common[1] - common[0];
-        else{
-            int i = 1;
-            while(true){
-                if(common[0] * i == common[1]){
-                    answer = i;
-                    break;
-                }
-                i++;
-            }
-            answer *= common[common.length - 1];
-        }
+        else
+            answer = common[common.length - 1] * (common[1] / common[0]);
+        
         return answer;
     }
 }
