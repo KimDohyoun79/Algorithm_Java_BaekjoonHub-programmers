0cbe6e569d8a441211808d5ee7c7bcac490cfacd
KimDohyoun79 <dogdo5726@gmail.com>
2023-10-03 15:03:39 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.72 ms, Memory: 91.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.HashMap;
+
+class Solution {
+    public String solution(String[] survey, int[] choices) {
+        //"RT", "TR", "FC", "CF", "MJ", "JM", "AN", "NA"
+        // "RT", CF", JM", "AN"
+        char[] cArr = {'R', 'T', 'C', 'F', 'J', 'M', 'A', 'N'};
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(char c : cArr)
+            map.put(c, 0);
+        System.out.println(map);
+
+        
+        for(int i = 0; i < choices.length; i++){
+        
+            if(choices[i] < 4){
+                int choice = choices[i] == 1 ? 3 : (choices[i] == 2 ? 2 : 1);
+                map.put(survey[i].charAt(0), map.get(survey[i].charAt(0)) + choice);
+            }
+            else if(choices[i] > 4){
+                map.put(survey[i].charAt(1), map.get(survey[i].charAt(1)) + choices[i] - 4);
+            }
+            
+        }
+        System.out.println(map);
+        
+        StringBuilder sb = new StringBuilder();
+        
+
+        for(int i = 0; i < 8; i+=2){
+            int x = map.get(cArr[i]);
+            int y = map.get(cArr[i + 1]);
+            sb.append(x >= y ? cArr[i] : cArr[i + 1]);
+        }
+        
+        return sb.toString();
+    }
+}
