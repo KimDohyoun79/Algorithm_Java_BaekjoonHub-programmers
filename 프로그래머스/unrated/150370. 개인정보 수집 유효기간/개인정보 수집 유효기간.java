5737a550e841ac5468752d3f03e77c848e0923b7
KimDohyoun79 <dogdo5726@gmail.com>
2023-10-03 10:07:38 +0900

    [unrated] Title: 개인정보 수집 유효기간, Time: 1.34 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -1,32 +1,28 @@
-import java.util.Arrays;
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.*;
 
 class Solution {
     public int[] solution(String today, String[] terms, String[] privacies) {
         
-        // 1. 기간 데이터 저장
+        // 기간 데이터 저장
         HashMap<Character, Integer> termsMap = new HashMap<>();
         for(String s : terms){
             termsMap.put(s.charAt(0), Integer.parseInt(s.substring(2, s.length())));
         }
         
-        // 2. 오늘 날짜(문자열)를 정수로 저장
+        System.out.println(termsMap);
+        
+        // 오늘 날짜 정수로 변환
         String[] todayStr = today.split("\\.");
         int[] todayDate = {Integer.parseInt(todayStr[0]), Integer.parseInt(todayStr[1]), Integer.parseInt(todayStr[2])};
         
-        // 3. 수집일자&약관 종류을 가지고 파기할 개인정보 구분
         ArrayList<Integer> list = new ArrayList<>();
         for(int i = 0; i < privacies.length; i++){
-            
-            // a. privacies 유효 기간 확인
             int term = termsMap.get(privacies[i].charAt(11));
-            
-            // b. privacies 개인정보 수집 일자를 정수형 배열에 저장
             String[] s = privacies[i].substring(0, 10).split("\\.");
             int[] termDate = {Integer.parseInt(s[0]), Integer.parseInt(s[1])+ term, Integer.parseInt(s[2]) - 1};
             
-            // c. 수집일자 + 유효기간
+            System.out.println("today : " + Arrays.toString(s));
+            System.out.println("before : " + Arrays.toString(termDate));
             if(termDate[2] == 0){
                 termDate[2] = 28;
                 termDate[1]--; 
@@ -34,9 +30,10 @@ class Solution {
             while(termDate[1] > 12){
                 termDate[1] -= 12;
                 termDate[0]++;
+                
             }
             
-            // d. 오늘 날짜와 비교하여 파기할 데이터 저장
+            System.out.println("after : " + Arrays.toString(termDate) +"\n");
             for(int j = 0; j < 3; j++){
                 if(todayDate[j] > termDate[j]){
                     list.add(i+1);
@@ -47,7 +44,7 @@ class Solution {
             }
         }
         
-        //4. ArrayList를 배열에 저장 후 정렬
+        // ArrayList를 배열에 저장 후 정렬
         int[] answer = new int[list.size()];
         for(int i = 0; i < list.size(); i++)
             answer[i] = list.get(i);
@@ -55,4 +52,6 @@ class Solution {
         
         return answer;
     }
+    
+    
 }
