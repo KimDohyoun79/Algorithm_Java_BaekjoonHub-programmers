43f953e3689cec76770dc8aca84bbb1e06acb5cd
KimDohyoun79 <dogdo5726@gmail.com>
2023-10-13 20:12:56 +0900

    [level unrated] Title: 커피 심부름, Time: 0.03 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181837. 커피 심부름/커피 심부름.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(String[] order) {
+        int answer = 0;
+        for(String s : order)
+            answer += s.contains("latte") ? 5000 : 4500;  
+        return answer;
+    }
+}
