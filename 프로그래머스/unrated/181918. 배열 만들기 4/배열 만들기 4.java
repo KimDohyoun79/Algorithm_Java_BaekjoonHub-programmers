a799a1aa263ac41521e8756352fcfc435a50a599
KimDohyoun79 <dogdo5726@gmail.com>
2023-10-13 20:49:12 +0900

    [level unrated] Title: 배열 만들기 4, Time: 4.74 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181918. 배열 만들기 4/배열 만들기 4.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr) {
+        
+        Stack<Integer> stack = new Stack<>();
+        
+        int i = 0;
+        while(i < arr.length){
+            if(stack.size() == 0)
+                stack.push(arr[i++]);
+            else if(stack.peek() < arr[i]){
+                stack.push(arr[i++]);
+            }
+            else
+                stack.pop();
+        }
+        
+        int[] stk = new int[stack.size()];
+        for(int j = stack.size() - 1; j >= 0; j--)
+            stk[j] = stack.pop();
+        
+        return stk;
+    }
+}
